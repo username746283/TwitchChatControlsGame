81e299e0d3fcc26387b37ef503e065b359646dfe
Jan9103 <55753387+Jan9103@users.noreply.github.com>
2021-09-04 22:12:25 +0200

    added the start.sh and cleaned up the start.bat

--- start.bat
+++ start.bat
@@ -1,12 +1,5 @@
 @echo off
 title Twitch Paladins Control
-::set path=<python-path.txt
-::if "" EQU "%path%" echo Write the filepath of python.exe into python-path.exe
-::if "" EQU "%path%" pause
-::if "" EQU "%path%" exit
-::if not exists "%path%" echo Write the filepath of python.exe into python-path.exe
-::if not exists "%path%" pause
-::if not exists "%path%" exit
 echo Starting..
 echo.
 python.exe TwitchPlays.py
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "Starting"
+python TwitchPlays.py
+echo "Ended"
