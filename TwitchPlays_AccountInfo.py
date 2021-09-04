b4210eb79a72bb788a8d64301ddf48d19bf39a3c
Jan9103 <55753387+Jan9103@users.noreply.github.com>
2021-09-04 21:33:11 +0200

    init

---
+++ TwitchPlays_AccountInfo.py
@@ -0,0 +1,9 @@
+# DougDoug Note:
+# This file contains your Twitch account info for logging into the chat
+# Your oauth-token can be generated at http://twitchapps.com/tmi/
+
+# WARNING: DO NOT PUBLICLY SHARE THIS INFORMATION. THIS INFO IS USED TO ACCESS YOUR TWITCH ACCOUNT.
+
+# TODO: put your Twitch username and OAuth token here
+TWITCH_USERNAME = 'TWITCH_CHANNELNAME_HERE'
+TWITCH_OAUTH_TOKEN = 'oauth:TWITCH_TOKEN_HERE'
